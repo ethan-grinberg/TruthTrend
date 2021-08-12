ec25d94ef7e4b0dd4d2f039ece4c319e537388b2
ethanbg2 <ethanbg2@illinois.edu>
2021-08-11 23:26:39 -0500

    made optimization for finding eps val

--- src/zinfo/article_clustering.py
+++ src/zinfo/article_clustering.py
@@ -3,7 +3,6 @@ import numpy as np
 
 # cluster algorithms
 from sklearn.cluster import DBSCAN
-from sklearn.neighbors import NearestNeighbors
 
 # nlp modules
 import spacy
@@ -35,18 +34,23 @@ def get_vectorized_titles(df):
 
 
 # finds optimal epsilon value for dbscan clustering
-def get_best_eps_val(vectors, neighbors=2, eps_factor=2):
-    neigh = NearestNeighbors(n_neighbors=neighbors)
-    nbrs = neigh.fit(vectors)
+# going off of the assumption that there should be about as many clusters as there are topics
+def get_best_eps_val(vectors, trending_news, start=2, end=50, step=2, decimal=100):
+    num_clusters = []
+    for i in [float(j) / decimal for j in range(start, end, step)]:
+        dbscan = DBSCAN(eps=i, min_samples=2, metric='cosine').fit(vectors)
+        # gets number of non -1 clusters
+        labels = dbscan.labels_
+        unique_vals = np.unique(labels)
+        unique_vals = unique_vals[1:]
+        num_clusters.append(len(unique_vals))
 
-    distances, indices = nbrs.kneighbors(vectors)
+    unique_topics = trending_news.topic.nunique()
+    difference_array = np.absolute(np.array(num_clusters) - unique_topics)
+    index = difference_array.argmin()
 
-    # return first non zero distance value divided by eps factor
-    distances = np.sort(distances, axis=0)
-    distances = distances[:, 1]
-    non_zero = distances.nonzero()
-
-    return distances[non_zero[0][0]] / eps_factor
+    best_eps_val = (start / decimal) + ((step / decimal) * index)
+    return best_eps_val
 
 
 def get_best_min_sample_val(num_total_articles, factor=132):
@@ -64,7 +68,7 @@ def cluster_articles(df):
     x = np.array(vectors)
 
     # finds best hyper parameters for dbscan
-    eps = get_best_eps_val(x)
+    eps = get_best_eps_val(x, df)
     min_articles = get_best_min_sample_val(len(df))
     print("eps_val: " + str(eps) + "\n" + "min_samples: " + str(min_articles))
 
